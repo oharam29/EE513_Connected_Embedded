fdf8a9d082764cbd4d37d41bc04626d1cde0c625
Michael <michael.ohara29@mail.dcu.ie>
2021-03-02 01:30:24 +0000

    updates

--- EE514_Assignment1/src/DS3231.cpp
+++ EE514_Assignment1/src/DS3231.cpp
@@ -2,8 +2,11 @@
 #include<fcntl.h>
 #include<sys/ioctl.h>
 #include<unistd.h>
+#include<linux/i2c.h>
 #include<linux/i2c-dev.h>
 #include<string>
+#include<iostream>
+#include<sstream>
 #include"DS3231.h"
 #define BUFFER_SIZE 19      //0x00 to 0x12
 using namespace std;
@@ -17,19 +20,23 @@ DS3231::DS3231(unsigned int bus, unsigned int device) {
 }
 
 
-int DS3231::open(){
+int DS3231::open()
+{
 	string name;
-	if(this->bus==0) name = "/dev/i2c-0";
+	if(this->bus==0){
+		name = "/dev/i2c-0";
+	}
 	else name = "/dev/i2c-1";
 
-	if((this->file::open(name.c_str(),O_RDWR)) < 0){
+	if((this->file::open(name.c_str(),O_RDWR)) < 0)
+	{
 		perror("I2C: failed to open the bus\n");
-			return 1;
+		return 1;
 	}
-
-	if(ioctl(this->file, I2C_SLAVE, this->device) < 0){
+	if(ioctl(this->file, I2C_SLAVE, this->device) < 0)
+	{
 		perror("I2C: Failed to connect to the device\n");
-			return 1;
+		return 1;
 	}
 	return 0;
 }
